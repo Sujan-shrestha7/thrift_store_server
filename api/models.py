ca608996a92d7f087bbe48fa83d2d4e07001b43d
Sujanshrestha <sujanstha5732@gmail.com>
2025-03-05 23:22:51 +0545

    tables

--- api/models.py
+++ api/models.py
@@ -14,3 +14,13 @@ class users(models.Model):
     class Meta:
         db_table = 'users'
 
+class admin(models.Model):
+    id = models.IntegerField(primary_key=True, null=False)
+    fullname = models.CharField(max_length=50, null=False, blank=False)
+    email =  models.CharField(max_length=100, null=False, blank=False)
+    password = models.CharField(max_length=20, null=False)
+
+    
+    class Meta:
+        db_table = 'admin'
+
