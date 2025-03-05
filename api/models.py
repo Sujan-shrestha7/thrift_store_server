43bc1e6bcc28161d2d57fc5e2cbf933016d076f5
Sujanshrestha <sujanstha5732@gmail.com>
2025-03-05 23:01:06 +0545

    table

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class users(models.Model):
+    id = models.IntegerField(primary_key=True, null=False)
+    fullname = models.CharField(max_length=50, null=False, blank=False)
+    address = models.CharField(max_length=100, null=False, blank=False)
+    email =  models.CharField(max_length=100, null=False, blank=False)
+    contact = models.BigIntegerField(null=False)
+    password = models.CharField(max_length=20, null=False)
+
+    
+    class Meta:
+        db_table = 'users'
+
