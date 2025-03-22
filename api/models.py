5afa64eeb1d601971ed5bbedec9badc133159d37
Sujanshrestha <sujanstha5732@gmail.com>
2025-03-23 04:12:48 +0545

    table

--- api/models.py
+++ api/models.py
@@ -5,11 +5,20 @@ from django.db import models
 class users(models.Model):
     fullname = models.CharField(max_length=50, null=False, blank=False)
     address = models.CharField(max_length=100, null=False, blank=False)
-    contact = models.BigIntegerField(null=False)
-    password = models.CharField(max_length=20, null=False)
-    
+    contact = models.CharField(
+        max_length=15, 
+        unique=True,
+        help_text="Required. 15 characters or fewer.",
+        error_messages={"unique": "A user with that contact already exists."},
+    )
+    password = models.CharField(max_length=255, null=True, blank=False)
+
     class Meta:
         db_table = 'users'
+
+    def __str__(self):
+        return f"Name: {self.fullname}, Address: {self.address}, Contact: {self.contact}"
+
 
 class admin(models.Model):
     fullname = models.CharField(max_length=50, null=False, blank=False)
@@ -23,6 +32,7 @@ class admin(models.Model):
 class products(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     category =  models.CharField(max_length=100, null=False, blank=False)
+    price = models.DecimalField(decimal_places=2,max_digits=10, null=True)
     usedtime = models.CharField(max_length=50, null=False)
     description =  models.CharField(max_length=255)
 
