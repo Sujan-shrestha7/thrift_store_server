a3c413a45395a165d2ba3fa3570512ef7d3a7498
Sujanshrestha <sujanstha5732@gmail.com>
2025-03-23 04:18:58 +0545

    table

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class users(models.Model):
+class Users(models.Model):
     fullname = models.CharField(max_length=50, null=False, blank=False)
     address = models.CharField(max_length=100, null=False, blank=False)
     contact = models.CharField(
@@ -20,16 +20,17 @@ class users(models.Model):
         return f"Name: {self.fullname}, Address: {self.address}, Contact: {self.contact}"
 
 
-class admin(models.Model):
+class Admin(models.Model):
     fullname = models.CharField(max_length=50, null=False, blank=False)
-    email =  models.CharField(max_length=100, null=False, blank=False)
+    email =  models.CharField(max_length=100,unique=True, null=False,
+        error_messages={"unique": "A user with that contact already exists."}, blank=False)
     password = models.CharField(max_length=20, null=False)
 
     
     class Meta:
         db_table = 'admin'
 
-class products(models.Model):
+class Products(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     category =  models.CharField(max_length=100, null=False, blank=False)
     price = models.DecimalField(decimal_places=2,max_digits=10, null=True)
@@ -39,3 +40,9 @@ class products(models.Model):
     class Meta:
         db_table = 'products'
 
+class Orders(models.Model):
+    userid = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='orders',null=True)
+    productid = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='products',null=True)
+
+    class Meta:
+        db_table = 'orders'
