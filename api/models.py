ef7ab39f30e06a58d733f082d54283762446762a
Sujanshrestha <sujanstha5732@gmail.com>
2025-03-22 15:35:42 +0545

    asf

--- api/models.py
+++ api/models.py
@@ -3,19 +3,15 @@ from django.db import models
 # Create your models here.
 
 class users(models.Model):
-    id = models.IntegerField(primary_key=True, null=False)
     fullname = models.CharField(max_length=50, null=False, blank=False)
     address = models.CharField(max_length=100, null=False, blank=False)
-    email =  models.CharField(max_length=100, null=False, blank=False)
     contact = models.BigIntegerField(null=False)
     password = models.CharField(max_length=20, null=False)
-
     
     class Meta:
         db_table = 'users'
 
 class admin(models.Model):
-    id = models.IntegerField(primary_key=True, null=False)
     fullname = models.CharField(max_length=50, null=False, blank=False)
     email =  models.CharField(max_length=100, null=False, blank=False)
     password = models.CharField(max_length=20, null=False)
@@ -25,7 +21,6 @@ class admin(models.Model):
         db_table = 'admin'
 
 class products(models.Model):
-    id = models.IntegerField(primary_key=True, null=False)
     name = models.CharField(max_length=100, null=False, blank=False)
     category =  models.CharField(max_length=100, null=False, blank=False)
     usedtime = models.CharField(max_length=50, null=False)
