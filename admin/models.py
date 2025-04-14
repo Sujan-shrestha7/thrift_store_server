3f14c0292d88e92a63395f1e8ba615814739e616
Sujanshrestha <sujanstha5732@gmail.com>
2025-04-14 23:55:02 +0545

    bil

--- admin/models.py
+++ admin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Admin(models.Model):
+    fullname = models.CharField(max_length=50, null=False, blank=False)
+    contact =  models.IntegerField(max_length=10,unique=True, null=False,
+        error_messages={"unique": "A user with that contact already exists."}, blank=False)
+    password = models.CharField(max_length=20, null=False)
+
+    
+    class Meta:
+        db_table = 'admin'
