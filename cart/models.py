e8df8eff7aa07b99b1bb1b0e3526cc4cec65dc22
Sujanshrestha <sujanstha5732@gmail.com>
2025-04-20 15:35:35 +0545

    asf

---
+++ cart/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Cart(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    category =  models.CharField(max_length=100, null=False, blank=False)
+    price = models.DecimalField(max_digits=5,decimal_places=2, null=True)
+    usedtime = models.CharField(max_length=50, null=False)
+    description =  models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'cart'
