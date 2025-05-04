e6a81c02594cc9e100a18cc05011bc3586e7f7ba
Sujanshrestha <sujanstha5732@gmail.com>
2025-05-04 08:58:19 +0545

    asf

--- cart/serializer.py
+++ cart/serializer.py
@@ -3,7 +3,8 @@ from .models import Cart
 from category.models import Category
 
 class CartSerializers(serializers.ModelSerializer):
-    
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    cat_name = serializers.CharField(source='category.cat_name', read_only=True)
     class Meta:
         model = Cart
-        fields = ['id','userid','name', 'category', 'price', 'usedtime', 'description']
+        fields = ['id','userid','name', 'category', 'price','cat_name', 'usedtime', 'description']
--- category/serializer.py
+++ category/serializer.py
@@ -3,8 +3,6 @@ from .models import Category
 from category.models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
-    cat_name = serializers.CharField(source='category.cat_name', read_only=True)
     class Meta:
         model = Category
         fields = '__all__'
